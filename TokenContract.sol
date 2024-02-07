e6391cbf36899e95c14a336299dbdd33f94eb887
kartikeyasureka <kartikeyasureka@gmail.com>
2024-02-07 12:11:52 +0530

    created a rewards system and updated the token contract

--- TokenContract.sol
+++ TokenContract.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract POSToken is ERC20, Ownable {
+    mapping(uint256 => address) public stakers;
+    uint256 public count = 0;
     event Stake(address indexed staker, uint256 value); // Added the Stake event definition
 
     constructor(uint initialSupply) ERC20("MoneyTrees", "MTM") Ownable(msg.sender) {
@@ -14,11 +16,17 @@ contract POSToken is ERC20, Ownable {
     function stake(uint256 value) public onlyOwner {
         require(value > 0, "Stake value must be greater than 0");
         _transfer(msg.sender, address(this), value);
+        stakers[count] = msg.sender;
+        count++;
         emit Stake(msg.sender, value);
     }
 
     function unstake(uint256 value) public onlyOwner {
         require(balanceOf(address(this)) >= value, "Insufficient staked tokens");
         _transfer(address(this), msg.sender, value);
+    }
+
+    function stakerLength() public view returns(uint256) {
+        return count;
     }
 }
